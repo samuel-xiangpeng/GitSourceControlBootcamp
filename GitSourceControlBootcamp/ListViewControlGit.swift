d62d7b713fd92ca90f10b22eaa5b8da2e39ba3e9
samuel-xiangpeng <samuel_xiangpeng@163.com>
2024-08-16 13:56:32 +0800

    [Feature] 初始化列表结构

---
+++ GitSourceControlBootcamp/ListViewControlGit.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewControlGit.swift
+//  GitSourceControlBootcamp
+//
+//  Created by 周祥鹏 on 2024/8/16.
+//
+
+import SwiftUI
+
+struct ListViewControlGit: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ListViewControlGit()
+}
